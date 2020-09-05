fac789e049bbe939612a0c6932e8ef01a9507d49
Lainera <fistlain@hotmail.com>
2020-09-05 15:55:32 -0700

    Move nat-instance config into submodule
    rm swap
    
    Add name tag

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "main" {
 }
 
 locals {
-  create_gateway = length(var.AMI_ID) == 0
+  use_managed_nat = length(var.AMI_ID) == 0
 }
 
 resource "aws_vpc" "main" {
@@ -77,65 +77,20 @@ resource "aws_route_table_association" "private" {
 // NGW
 module "nat_gateway" {
   source = "./nat-gateway"
-  count = local.create_gateway ? 1 : 0
+  count = local.use_managed_nat ? 1 : 0
   depends_on    = [aws_internet_gateway.igw]
   SUBNET_ID = aws_subnet.public[0].id
   ROUTE_TABLE_ID = aws_route_table.private.id
 }
 
 // Instance
-resource "aws_instance" "nat_instance" {
-  count = local.create_gateway ? 0 : 1
+module "nat_instance" {
+  source = "./nat-instance"
+  count = local.use_managed_nat ? 0 : 1
   depends_on    = [aws_internet_gateway.igw]
-  ami = var.AMI_ID
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [ aws_security_group.nat_instance[0].id ]
-  subnet_id = aws_subnet.public[0].id
-  source_dest_check = false
-
-  credit_specification {
-    cpu_credits = "standard"
-  }
-}
-
-resource "aws_route" "private_nat_instance_routing" {
-  count = local.create_gateway ? 0 : 1
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  instance_id         = aws_instance.nat_instance[0].id
-}
-
-resource "aws_security_group" "nat_instance" {
-  count = local.create_gateway ? 0 : 1
-  name_prefix = "nat-instance-"
-  description = "Security group for nat instance: allow local Ingress any Egress"
   
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "nat-instance-sg"
-  }
+  AMI_ID = var.AMI_ID
 
-  ingress {
-    description = "HTTPS from within VPC"
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
-  }
-
-  ingress {
-    description = "HTTP from within VPC"
-    from_port = 80
-    to_port = 80 
-    protocol = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
-  }
- 
-  egress {
-    description = "Any egress"
-    from_port = 0
-    to_port = 0 
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  } 
+  SUBNET_ID = aws_subnet.public[0].id
+  ROUTE_TABLE_ID = aws_route_table.private.id
 }
---
+++ nat-instance/main.tf
@@ -0,0 +1,83 @@
+variable "AMI_ID" {
+  type = string
+  description = "NAT instance AMI-ID, if not passed creates NAT gateway instead."
+}
+
+variable "SUBNET_ID" {
+  type = string
+  description = "Public subnet to spawn instance in"
+}
+
+variable "ROUTE_TABLE_ID" {
+  type = string
+  description = "Route table to associate route with"
+}
+
+data "aws_route_table" "private" {
+  route_table_id = var.ROUTE_TABLE_ID
+} 
+
+data "aws_subnet" "public" {
+  id = var.SUBNET_ID
+}
+
+data "aws_vpc" "main" {
+  id = data.aws_subnet.public.vpc_id
+}
+
+resource "aws_instance" "nat_instance" {
+  ami = var.AMI_ID
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ aws_security_group.nat_instance.id ]
+  subnet_id = data.aws_subnet.public.id
+  source_dest_check = false
+
+  credit_specification {
+    cpu_credits = "standard"
+  }
+
+  tags = {
+    Name = "nat-instance"
+  }
+}
+
+resource "aws_route" "private_nat_instance_routing" {
+  route_table_id         = data.aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  instance_id         = aws_instance.nat_instance.id
+}
+
+resource "aws_security_group" "nat_instance" {
+  name_prefix = "nat-instance-"
+  description = "Security group for nat instance: allow local Ingress any Egress"
+  
+  vpc_id = data.aws_vpc.main.id
+  tags = {
+    Name = "nat-instance-sg"
+  }
+
+  ingress {
+    description = "HTTPS from within VPC"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = [data.aws_vpc.main.cidr_block]
+  }
+
+  ingress {
+    description = "HTTP from within VPC"
+    from_port = 80
+    to_port = 80 
+    protocol = "tcp"
+    cidr_blocks = [data.aws_vpc.main.cidr_block]
+  }
+ 
+  egress {
+    description = "Any egress"
+    from_port = 0
+    to_port = 0 
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+}
+
